4160b108f30733761f86832932b3a487c6da73e5
Bartek Krawczyk <bartoszkrawczykk@gmail.com>
2023-01-31 19:48:27 +0100

    Rozwiązanie

---
+++ src/main/java/pl/javastart/task/Drink.java
@@ -0,0 +1,9 @@
+package pl.javastart.task;
+
+class Drink {
+    String name;
+    double price;
+    boolean alcohol;
+    String ingredient;
+    int capacity;
+}
--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,6 +3,31 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
+        Drink mojito = new Drink();
+        mojito.name = "Mojito";
+        mojito.price = 13.0;
+        mojito.alcohol = true;
 
+        Drink mojito1 = new Drink();
+        mojito1.ingredient = "woda\n";
+        mojito1.capacity = 70;
+
+        Drink mojito2 = new Drink();
+        mojito2.ingredient = "sok cytrynowy\n";
+        mojito2.capacity = 70;
+
+        Drink mojito3 = new Drink();
+        mojito3.ingredient = "wódka";
+        mojito3.capacity = 70;
+
+
+        String fullIngredient = (mojito1.ingredient + mojito2.ingredient + mojito3.ingredient);
+        int fullcapacity = (mojito1.capacity + mojito2.capacity + mojito3.capacity);
+
+        System.out.printf("Drink: %s%n", mojito.name);
+        System.out.printf("Cena: %.1fzł%n", mojito.price);
+        System.out.printf("Czy zawiera alkohol? %b%n", mojito.alcohol);
+        System.out.printf("Składniki:%n%s%n", fullIngredient);
+        System.out.printf("Pojemność: %dml", fullcapacity);
     }
 }
